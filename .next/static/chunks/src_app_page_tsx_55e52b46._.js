121167fcc0b8f07d53b24fb599240dd77e352972
mynameisthevano <118809223+mynameisthevano@users.noreply.github.com>
2025-07-03 18:35:50 +0700

    Add .next/static/chunks/src_app_page_tsx_55e52b46._.js 1751542548698

---
+++ .next/static/chunks/src_app_page_tsx_55e52b46._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_142b5365._.js"
+],
+    source: "dynamic"
+});
